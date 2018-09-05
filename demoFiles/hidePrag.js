f014cd0c7d15f4bd575e6fe20a2aefdf657f22e5
Nabiji Noor <noornabiji4@gmail.com>
2018-09-05 20:10:25 +0530

    JavaScript file for hide [paragraph

---
+++ demoFiles/hidePrag.js
@@ -0,0 +1,11 @@
+$('#click').on('click' ,function(){
+    $('#hide').hide();
+});
+
+$('#show').on('click', function(){
+    $('#hide').show();
+});
+
+$('.x').on('click' ,function(){
+    $('#hide').hide();
+});
